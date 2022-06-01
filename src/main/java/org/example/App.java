60738fc4e0a15ad1fd9db1c1ad58e073a4b53763
fatiha.beqirovski <fatiha.beqirovski@trendyol.com>
2022-06-01 10:35:59 +0200

    add prime factors

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,5 +1,8 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Hello world!
  *
@@ -8,6 +11,27 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        List<Integer> list = primeFactors(18);
+
+        for (int i = 0; i < list.size(); i++) {
+            System.out.print(list.get(i) + " ");
+        }
+
+    }
+
+    public static List<Integer> primeFactors(int num){
+        List<Integer> list = new ArrayList<Integer>();
+        for(int i=2; i<num; i++){
+            while(num%i==0){
+                list.add(i);
+                num = num/i;
+            }
+        }
+
+        if(num>2){
+            list.add(num);
+        }
+
+        return list;
     }
 }
